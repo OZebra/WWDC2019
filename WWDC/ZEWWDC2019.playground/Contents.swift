5d3c561ec47c9f84ddd8ebe1cde554ddcb0e9877
jose <jose@renanzinho.local>
2019-03-20 21:33:18 -0300

    sei n

---
+++ WWDC/ZEWWDC2019.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: ## Zee
+//: ### A quick travel through the physics world!
+//: ![The real head of the household?](blinkzee.png)
+import PlaygroundSupport
+import SpriteKit
+
+// Load the SKScene from 'GameScene.sks'
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 800, height: 480))
+if let scene = GameScene(fileNamed: "GameScene") {
+    // Set the scale mode to scale to fit the window
+    scene.scaleMode = .aspectFill
+    
+    // Present the scene
+    sceneView.presentScene(scene)
+}
+
+PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
